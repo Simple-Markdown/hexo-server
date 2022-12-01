4c72d95408f32b39ab985659a57b2dd81e32982b
microDreamSheep <atthemc313128460@gmail.com>
2022-12-01 10:24:26 +0800

    导包

--- src/main/java/live/midreamsheep/hexo/server/data/Constant.java
+++
@@ -1,6 +0,0 @@
-package live.midreamsheep.hexo.server.data;
-
-public class Constant {
-    public static final String cachePath = "//workplace";
-    public static final String blogPath = "//blogs";
-}
--- src/main/java/live/midreamsheep/hexo/server/server/ServerController.java
+++ src/main/java/live/midreamsheep/hexo/server/server/ServerController.java
@@ -1,16 +1,10 @@
 package live.midreamsheep.hexo.server.server;
 
 import live.midreamsheep.hexo.server.config.Config;
-import live.midreamsheep.hexo.server.data.Constant;
-import live.midreamsheep.hexo.server.monitor.FileListener;
-import live.midreamsheep.hexo.server.monitor.MonitorStarter;
 
 public class ServerController {
     boolean isStart = true;
     public void start() throws Exception {
-        //从云端获取文件覆盖本地文件
-        MonitorStarter monitorStarter = new MonitorStarter(1000);
-        monitorStarter.monitor(Config.nativeHexoPath+ Constant.blogPath, new FileListener());
-        monitorStarter.start();
+
     }
 }
