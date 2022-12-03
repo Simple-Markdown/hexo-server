3f1b49ac8c7e05c675023d8f2ca0d69f72149799
microDreamSheep <atthemc313128460@gmail.com>
2022-12-03 14:31:21 +0800

    优化

--- src/main/java/live/midreamsheep/hexo/server/server/ServerController.java
+++ src/main/java/live/midreamsheep/hexo/server/server/ServerController.java
@@ -6,8 +6,10 @@ public class ServerController {
     boolean isStart = true;
     public void start() throws Exception {
         Connector.init();
+        System.out.println("服务器已启动");
         while (isStart){
             new Thread(new ServerHandler(Connector.serverSocketChannel.accept())).start();
+            System.out.println("一个客户端已连接");
         }
     }
 }
